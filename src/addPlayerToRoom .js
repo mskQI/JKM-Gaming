e5b2d6253ed6c08451313565fceafb0b7b789cea
mskQI <156182962+mskQI@users.noreply.github.com>
2024-06-14 17:05:38 +0200

    deleted some files

--- src/addPlayerToRoom .js
+++
@@ -1,8 +0,0 @@
-const addPlayerToRoom = async (roomId, player) => {
-    const roomRef = db.collection('rooms').doc(roomId);
-    await roomRef.update({
-      players: firebase.firestore.FieldValue.arrayUnion(player)
-    });
-    console.log(`Player added to room: ${roomId}`);
-  };
-  
